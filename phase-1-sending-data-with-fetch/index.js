7bcc63f7935de96dc07b36d242f46dc36319437b
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-05-04 22:57:11 +0300

    phase-1-sending-data-with-fetch completed

---
+++ phase-1-sending-data-with-fetch/index.js
@@ -0,0 +1,16 @@
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => document.querySelector("body").append(data.id))
+    .catch((message) => document.querySelector("body").append(message));
+}
