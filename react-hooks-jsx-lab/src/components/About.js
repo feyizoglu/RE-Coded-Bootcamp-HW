69f9547dc799588b7e771c051ffcd23cac996ffb
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-06-04 23:48:49 +0300

    react-hooks-jsx-lab completed

---
+++ react-hooks-jsx-lab/src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { image } from "../data/data";
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About me</h2>
+      <p>I am Here</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
+}
+
+export default About;
