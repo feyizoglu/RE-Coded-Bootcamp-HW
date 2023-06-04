4e73c337573fe70c8d088e4c04965dac560a5c6e
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-06-05 01:06:46 +0300

    react-hooks-props-basics-lab completed

---
+++ react-hooks-props-basics-lab/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Links from "./Links";
+import user from "../data/user";
+
+function About(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {props.bio ? <p>{props.bio}</p> : ""}
+
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
+    </div>
+  );
+}
+
+export default About;
