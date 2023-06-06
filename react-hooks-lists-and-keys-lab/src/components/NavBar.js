c81a730c96dc3bb3d0013ccfcae8d1435e65df10
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-06-06 17:17:43 +0300

    react-hooks-lists-and-keys-lab completed

---
+++ react-hooks-lists-and-keys-lab/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+
+  return (
+    <nav>
+      {links.map((link, index) => {
+        return (
+          <a key={index} href={`#${link}`}>
+            {link}
+          </a>
+        );
+      })}
+    </nav>
+  );
+}
+
+export default NavBar;
