87adc4713df547b19ad972987ef44f99175761a2
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-06-07 10:12:15 +0300

    react-hooks-event-handling-lab completed

---
+++ react-hooks-event-handling-lab/src/components/Keypad.js
@@ -0,0 +1,14 @@
+// Code Keypad Component Here
+
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={() => console.log("Entering password...")}
+      />
+    </div>
+  );
+}
+
+export default Keypad;
