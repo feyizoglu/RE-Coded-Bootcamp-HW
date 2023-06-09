5a16a763957d2669e4d0144425f05e48f7ca3778
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-06-09 09:44:03 +0300

    react-hooks-information-flow-code-along

---
+++ react-hooks-information-flow-code-along/src/Child.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
+
+function Child({ onChangeColor, color }) {
+  const handleClick = () => {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
+}
+
+export default Child;
---
+++ react-hooks-information-flow-code-along/src/Parent.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
+import Child from "./Child";
+
+function Parent() {
+  const randomColor = getRandomColor();
+  const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  const handleChangeColor = (newChildColor) => {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  };
+
+  return (
+    <div className="parent" style={{ backgroundColor: color }}>
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+    </div>
+  );
+}
+
+export default Parent;
